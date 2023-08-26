fc323c5979f2ec55c567788a70b087c30e10dba6
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-26 20:21:49 -0300

    feat: send files

---
+++ src/main/java/com/labi/typing/TypingApplication.java
@@ -0,0 +1,13 @@
+package com.labi.typing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypingApplication.class, args);
+	}
+
+}
