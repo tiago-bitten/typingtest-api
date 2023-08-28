fd60ea62d4a252582f3f2275577a3f81eaee6cfa
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-27 23:56:10 -0300

    feat: add ModelMapperUtil in util

--- src/main/java/com/labi/typing/util/ModelMapperUtil.java
+++ src/main/java/com/labi/typing/util/ModelMapperUtil.java
@@ -1,2 +1,13 @@
-package com.labi.typing.util;public class ModelMapperUtil {
+package com.labi.typing.util;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.ui.ModelMap;
+
+public class ModelMapperUtil {
+
+    private static final ModelMapper modelMapper = new ModelMapper();
+
+    public static <D, T> D map(T entity, Class<D> outClass) {
+        return modelMapper.map(entity, outClass);
+    }
 }
