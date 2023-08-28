75fe3498cad392ef06afbc9d8f67d66346fa3787
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-28 02:28:34 -0300

    feat: add MethodArgumentNotValidException class

--- src/main/java/com/labi/typing/DTO/UserRegisterDTO.java
+++ src/main/java/com/labi/typing/DTO/UserRegisterDTO.java
@@ -3,17 +3,17 @@ package com.labi.typing.DTO;
 import com.labi.typing.enums.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.NonNull;
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 public record UserRegisterDTO(
-        @NonNull @Length(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+        @NotBlank(message = "Username is required") @Length(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
         String username,
-        @NonNull @Email(message = "Invalid email address")
+        @NotBlank(message = "Email is required") @Email(message = "Invalid email address")
         String email,
-        @NonNull @Length(min = 6, message = "Password must be at least 6 characters")
+        @NotBlank(message = "Password is required") @Length(min = 6, message = "Password must be at least 6 characters")
         String password,
-        @NotBlank(message = "Role must be informed")
+        @NotBlank(message = "Role is required")
         UserRole userRole
 ) {
 }
--- src/main/java/com/labi/typing/exception/GlobalHandlerException.java
+++ src/main/java/com/labi/typing/exception/GlobalHandlerException.java
@@ -5,6 +5,7 @@ import com.labi.typing.exception.custom.UsernameAlreadyExistsException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -27,6 +28,19 @@ public class GlobalHandlerException {
                 errors
         );
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        log(ex.getClass().getName() + " was thrown");
+        Set<Message> errors = Set.of(new Message(ex.getBindingResult().getFieldError().getDefaultMessage()));
+        ApiError apiError = new ApiError(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                request.getRequestURI(),
+                errors
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiError);
     }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
