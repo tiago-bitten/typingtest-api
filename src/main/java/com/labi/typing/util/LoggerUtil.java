d8008120adfa0c8048609c07342a10e12f8b2505
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-28 00:59:19 -0300

    feat: add LoggerUtil class in util

---
+++ src/main/java/com/labi/typing/util/LoggerUtil.java
@@ -0,0 +1,13 @@
+package com.labi.typing.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggerUtil.class);
+
+    public static void log(String message) {
+        logger.info(message);
+    }
+}
