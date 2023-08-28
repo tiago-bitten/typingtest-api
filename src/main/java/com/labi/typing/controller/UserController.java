ae01b82fa4ffc094fde5c2187a4b9ac8e9432597
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-28 02:02:01 -0300

    feat: add register user rote

--- src/main/java/com/labi/typing/controller/UserController.java
+++ src/main/java/com/labi/typing/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.labi.typing.controller;
 
+import com.labi.typing.DTO.UserRegisterDTO;
+import com.labi.typing.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
 
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody UserRegisterDTO userRegisterDTO) {
+        userService.saveUser(userRegisterDTO);
+        return ResponseEntity.noContent().build();
+    }
 }
