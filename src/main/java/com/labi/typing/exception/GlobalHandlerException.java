37636243f4aaf119b2656f96730aafe5512cb36e
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-28 01:22:21 -0300

    feat: add UsernameAlreadyExistsException class

--- src/main/java/com/labi/typing/exception/GlobalHandlerException.java
+++ src/main/java/com/labi/typing/exception/GlobalHandlerException.java
@@ -1,6 +1,7 @@
 package com.labi.typing.exception;
 
 import com.labi.typing.exception.custom.EmailAlreadyExistsException;
+import com.labi.typing.exception.custom.UsernameAlreadyExistsException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,15 +9,26 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
-import java.util.List;
 import java.util.Set;
 
 @ControllerAdvice
 public class GlobalHandlerException {
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<ApiError> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex, HttpServletRequest request) {
+    public ResponseEntity<ApiError> handleEmailAlreadyExistsException(HttpServletRequest request) {
         Set<Message> errors = Set.of(new Message("Email already exists"));
+        ApiError apiError = new ApiError(
+                Instant.now(),
+                HttpStatus.CONFLICT.value(),
+                request.getRequestURI(),
+                errors
+        );
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(apiError);
+    }
+
+    @ExceptionHandler(UsernameAlreadyExistsException.class)
+    public ResponseEntity<ApiError> handleUsernameAlreadyExistsException(HttpServletRequest request) {
+        Set<Message> errors = Set.of(new Message("Username already exists"));
         ApiError apiError = new ApiError(
                 Instant.now(),
                 HttpStatus.CONFLICT.value(),
