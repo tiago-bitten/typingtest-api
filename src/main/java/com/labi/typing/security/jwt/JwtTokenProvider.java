72c4224079f028c16fe40bc48ae7834814610107
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-30 17:36:30 -0300

    feat: add JwtTokenProvider class

---
+++ src/main/java/com/labi/typing/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,50 @@
+package com.labi.typing.security.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generateToken(String username) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("labi")
+                    .withSubject(username)
+                    .withExpiresAt(new Date(System.currentTimeMillis() + expiration))
+                    .sign(algorithm);
+
+            return token;
+        }
+        catch (JWTCreationException e) {
+            throw new RuntimeException(e + "Error while creating JWT token");
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("labi")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }
+        catch (JWTVerificationException e) {
+            return "";
+        }
+    }
+}
