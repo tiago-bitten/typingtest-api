3101e3ab3914bdc7263ba2a8d8aeed8241d9f69d
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-30 23:57:27 -0300

    refactor: remove JwtTokenProvider dependency injection in UserController class

--- src/main/java/com/labi/typing/controller/UserController.java
+++ src/main/java/com/labi/typing/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.labi.typing.controller;
 
 import com.labi.typing.DTO.UserRegisterDTO;
-import com.labi.typing.security.jwt.JwtTokenProvider;
 import com.labi.typing.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +18,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private JwtTokenProvider jwtTokenProvider;
-
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody @Valid UserRegisterDTO userRegisterDTO) {
         userService.saveUser(userRegisterDTO);
-        String token = jwtTokenProvider.generateToken(userRegisterDTO.username());
-        return new ResponseEntity<>(token, HttpStatus.CREATED);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
