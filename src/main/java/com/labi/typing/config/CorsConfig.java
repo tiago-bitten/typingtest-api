4b72b69f6436ff9db26be401565e40e9dc3e8e5a
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-31 14:03:42 -0300

    fix: add all allowedOriginPatterns

--- src/main/java/com/labi/typing/config/CorsConfig.java
+++ src/main/java/com/labi/typing/config/CorsConfig.java
@@ -13,6 +13,6 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedOriginPatterns("*");
     }
 }
