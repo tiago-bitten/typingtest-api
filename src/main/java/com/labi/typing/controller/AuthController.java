6727ddfa67b6634c895e8f46ff46e2fd2b7f9079
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-08-30 23:37:45 -0300

    feat: add endpoint for user authentication

--- src/main/java/com/labi/typing/controller/AuthController.java
+++ src/main/java/com/labi/typing/controller/AuthController.java
@@ -6,6 +6,8 @@ import com.labi.typing.security.jwt.JwtTokenProvider;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +20,15 @@ public class AuthController {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
-//    @PostMapping("/login")
-//    public ResponseEntity<TokenDTO> login(@RequestBody @Valid AuthenticationDTO authenticationDTO) {
-//
-//    }
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDTO> login(@RequestBody @Valid AuthenticationDTO authenticationDTO) {
+        var usernamepassword = new UsernamePasswordAuthenticationToken(authenticationDTO.username(), authenticationDTO.password());
+        var auth = authenticationManager.authenticate(usernamepassword);
+        var token = jwtTokenProvider.generateToken(auth.getName());
+
+        return ResponseEntity.ok(new TokenDTO(token));
+    }
 }
