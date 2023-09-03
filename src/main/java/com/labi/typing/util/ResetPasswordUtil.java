2801cbf50b1caf65f8b46681c8bb0afec40ecbf2
tiago-bitten <tiago.sbittencourt@icloud.com>
2023-09-03 13:56:47 -0300

    feat: add ResetPasswordUtil class

--- src/main/java/com/labi/typing/util/ResetPasswordUtil.java
+++ src/main/java/com/labi/typing/util/ResetPasswordUtil.java
@@ -1,10 +1,28 @@
 package com.labi.typing.util;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
-public class RandomUtil {
+public class ResetPasswordUtil {
 
-    private Random random;
+    private static final String LETTERS_LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String LETTERS_UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String NUMBERS = "0123456789";
+    private static final String SPECIAL_CHARACTERS = "!@#$%";
 
-    private static final String LETTERS_LOWER_CASE = "ab"
+    private static final String ALL_CHARACTERS =
+            LETTERS_LOWER_CASE + LETTERS_UPPER_CASE + NUMBERS + SPECIAL_CHARACTERS;
+
+    private static final SecureRandom random = new SecureRandom();
+
+    public static String generateRandomPassword(int length) {
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(ALL_CHARACTERS.length());
+            char randomChar = ALL_CHARACTERS.charAt(randomIndex);
+            password.append(randomChar);
+        }
+
+        return password.toString();
+    }
 }
