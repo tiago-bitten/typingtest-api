71fa0332aba94b264aeeeb3685f1e3451ac4af9a
BuildTools <tiago.sbittencourt@icloud.com>
2023-09-04 02:17:03 -0300

    fix: passwordencoder in userservice

--- src/main/java/com/labi/typing/service/UserService.java
+++ src/main/java/com/labi/typing/service/UserService.java
@@ -9,7 +9,7 @@ import com.labi.typing.security.jwt.JwtTokenProvider;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -28,7 +28,7 @@ public class UserService {
     private JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    private BCryptPasswordEncoder bcrypt;
+    private PasswordEncoder bcrypt;
 
     @Autowired
     private EmailService emailService;
